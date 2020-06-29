1696ae7df1b6cf88ff0e5e80a860a25d5bfda7b5
Tyler Fink <tfink419@gmail.com>
2020-06-28 22:29:19 -0600

    undo webpacker change, shits broken

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-environment.config.output.path = environment.config.output.path.replace(/packs-dev/, 'packs')
-environment.config.output.publicPath = environment.config.output.publicPath.replace(/packs-dev/, 'packs')
-
 module.exports = environment.toWebpackConfig()
