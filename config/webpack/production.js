25ac39cea2220ebab3ced0f46c63571c250417ef
Tyler Fink <tfink419@gmail.com>
2020-06-28 22:34:43 -0600

    Redo webpack change, but i have to manually update the manifest

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.config.output.path = environment.config.output.path.replace(/packs-dev/, 'packs')
+environment.config.output.publicPath = environment.config.output.publicPath.replace(/packs-dev/, 'packs')
+
+
+console.log('*************************************************')
+console.log(environment)
+console.log('*************************************************')
+
+
 module.exports = environment.toWebpackConfig()
