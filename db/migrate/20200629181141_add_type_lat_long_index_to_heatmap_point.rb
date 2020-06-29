c5ae088f49de1fb4bde9df12056d480aeaaca0de
Tyler Fink <tfink419@gmail.com>
2020-06-29 16:21:05 -0600

    fix db migration

--- db/migrate/20200629181141_add_type_lat_long_index_to_heatmap_point.rb
+++ db/migrate/20200629181141_add_type_lat_long_index_to_heatmap_point.rb
@@ -1,7 +1,7 @@
 class AddTypeLatLongIndexToHeatmapPoint < ActiveRecord::Migration[5.2]
   def change
-    remove_index :heatmap_points, "index_heatmap_points_on_type_and_lat"
-    remove_index :heatmap_points, "index_heatmap_points_on_type_and_long"
+    remove_index :heatmap_points, name: "index_heatmap_points_on_type_and_lat"
+    remove_index :heatmap_points, name: "index_heatmap_points_on_type_and_long"
     add_index :heatmap_points, [:transit_type, :precision, :lat], name: "index_heatmap_points_on_type_and_lat"
     add_index :heatmap_points, [:transit_type, :precision, :long], name: "index_heatmap_points_on_type_and_long"
     add_index :heatmap_points, [:transit_type, :lat, :long], name: "index_heatmap_points_on_type_lat_long"
