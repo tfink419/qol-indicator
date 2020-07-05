59ea5d2f4f5e2145fc70132c9e0a74ed8c58514b
Tyler Fink <tfink419@gmail.com>
2020-07-04 19:44:12 -0600

    Move heatmap render to separate component

--- app/controllers/map_data_controller.rb
+++ app/controllers/map_data_controller.rb
@@ -5,17 +5,19 @@ class MapDataController < ApplicationController
   def retrieve_map_data
     south_west = JSON.parse(params[:south_west])
     north_east = JSON.parse(params[:north_east])
-    gstores = GroceryStore.where_in_coordinate_range(south_west, north_east).limit(1000)
+    gstores = GroceryStore.where_in_coordinate_range(south_west, north_east).limit(1000).pluck(:id, :lat, :long)
     isochrones = []
 
     transit_type = params[:transit_type] ? params[:transit_type] : User.find(session[:user_id]).map_preferences.transit_type
 
-    hmp = HeatmapPoint.where(transit_type: transit_type).where_in_coordinate_range(south_west, north_east, params[:zoom]).limit(100000)
+    hmp = HeatmapPoint.where(transit_type: transit_type)\
+    .where_in_coordinate_range(south_west, north_east, params[:zoom]).limit(100000)\
+    .order(:lat, :long).pluck(:lat, :long, :quality)
 
     render :json => { 
       :status => 0,
-      :grocery_stores => gstores.pluck(:id, :lat, :long),
-      :heatmap_points => hmp.pluck(:lat, :long, :quality)
+      :grocery_stores => gstores,
+      :heatmap_points => hmp
     }
   end
 
