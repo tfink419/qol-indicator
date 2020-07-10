20ceb6bb769eb8ab1b6de9aeb8c8824a4ca3acb6
Tyler Fink <tfink419@gmail.com>
2020-07-10 17:28:40 -0600

    Convert from Mapbox to Google Maps

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.insert(
+  "Environment",
+  new webpack.DefinePlugin({
+    'GOOGLE_KEY': JSON.stringify(process.env.GOOGLE_KEY)
+  })
+)
 
 module.exports = environment
