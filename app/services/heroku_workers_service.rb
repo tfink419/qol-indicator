95e0c8ef4095ac19dc65725e7b7213baafb95943
Tyler Fink <tfink419@gmail.com>
2020-07-14 22:05:26 -0600

    Move heroku to instance

--- app/services/heroku_workers_service.rb
+++ app/services/heroku_workers_service.rb
@@ -1,16 +1,18 @@
 require 'platform-api'
 
-heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
-
 class HerokuWorkersService
   HEROKU_APP_NAME = 'qol-indicator-workers'
   HEROKU_PROC_NAME = 'worker'
   NUM_WORKERS = 9
+
+  def initialize
+    @heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
+  end
   def self.start
-    heroku.formation.update(HEROKU_APP_NAME, HEROKU_PROC_NAME, { quantity: NUM_WORKERS })
+    @heroku.formation.update(HEROKU_APP_NAME, HEROKU_PROC_NAME, { quantity: NUM_WORKERS })
   end
 
   def self.stop
-    heroku.formation.update(HEROKU_APP_NAME, HEROKU_PROC_NAME, { quantity: 0 })
+    @heroku.formation.update(HEROKU_APP_NAME, HEROKU_PROC_NAME, { quantity: 0 })
   end
 end
