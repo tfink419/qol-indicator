65215c84bb9161eb33d0cb28000fc383440e2f76
Tyler Fink <tfink419@gmail.com>
2020-07-23 13:29:26 -0600

    fix google web key for each environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,9 @@ const webpack = require('webpack')
 environment.plugins.insert(
   "Environment",
   new webpack.DefinePlugin({
-    'GOOGLE_WEB_KEY': JSON.stringify(process.env.GOOGLE_WEB_KEY)
+    'GOOGLE_WEB_KEY': process.env.NODE_ENV == 'production' ? 
+      JSON.stringify(process.env.GOOGLE_PROD_WEB_KEY)
+      : JSON.stringify(process.env.GOOGLE_WEB_KEY)
   })
 )
 
