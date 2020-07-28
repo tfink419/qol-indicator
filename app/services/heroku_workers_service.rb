3d2a4261c52aa21540071c1ad3efc84f43269aaa
Tyler Fink <tfink419@gmail.com>
2020-07-28 17:11:45 -0600

    convert env to i

--- app/services/heroku_workers_service.rb
+++ app/services/heroku_workers_service.rb
@@ -3,7 +3,7 @@ require 'platform-api'
 class HerokuWorkersService
   HEROKU_APP_NAME = 'qol-indicator-workers'
   HEROKU_PROC_NAME = 'worker'
-  NUM_WORKERS = (ENV['NUM_HEATMAP_THREADS'] || 8) + 1
+  NUM_WORKERS = (ENV['NUM_HEATMAP_THREADS'] || 8).to_i + 1
 
   def initialize
     @heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
