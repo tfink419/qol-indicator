d6d3c77dca848ac85d1c029a59433d68d9cd986d
Tyler Fink <tfink419@gmail.com>
2020-08-11 20:57:27 -0600

    Fix for last change

--- app/services/data_image_service.rb
+++ app/services/data_image_service.rb
@@ -3,13 +3,13 @@ require 'fileutils'
 class DataImageService
   DATA_CHUNK_SIZE = 256
   USE_AWS_S3 = Rails.env == 'production' || ENV['USE_AWS_S3']
+  BUCKET = 'my-qoli-data'
 
   def initialize(map_point_type, zoom)
     @zoom = zoom
     @map_point_type = map_point_type
     if USE_AWS_S3
       @s3_client = Aws::S3::Client.new(region: 'us-west-2')
-      @bucket = 'my-qoli-data'
     end
   end
 
@@ -21,14 +21,14 @@ class DataImageService
 
   def presigned_url_put(extra_details, lat_sector, lng_sector)
     signer = Aws::S3::Presigner.new(client:@s3_client)
-    signer.presigned_url(:put_object, bucket: "my-qoli-data", key: path)
+    signer.presigned_url(:put_object, bucket: BUCKET, key: get_path(extra_details, lat_sector, lng_sector))
   end
 
   def save(extra_details, lat_sector, lng_sector, data)
     return if data.nil?
     file_path = get_path(extra_details, lat_sector, lng_sector)
     if USE_AWS_S3
-      @s3_client.put_object(bucket: @bucket, key: "#{dir}/#{filename}", body:data)
+      @s3_client.put_object(bucket: BUCKET, key: "#{dir}/#{filename}", body:data)
     else
       puts "Saving: '#{file_path}'"
       dir = "#{Rails.root}/quality_map_image_data/#{dir}"
@@ -41,7 +41,7 @@ class DataImageService
     file_path = get_path(extra_details, lat_sector, lng_sector)
     if USE_AWS_S3
       @s3_client.get_object({
-        bucket: @bucket, 
+        bucket: BUCKET, 
         key: file_path, 
       }).body.read rescue nil
     else
