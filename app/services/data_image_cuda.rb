a059ea93619b018e505bf7de5edf72efbf7c835b
Tyler Fink <tfink419@gmail.com>
2020-08-11 20:35:03 -0600

    CUDA Service

---
+++ app/services/data_image_cuda.rb
@@ -0,0 +1,36 @@
+class DataImageCuda
+  METHOD_MAP = {
+    "LogExpSum" => 0,
+    "First" => 1
+  }
+  REDIS_QUEUE_NAME = "data_image_qda:queue"
+  REDIS_STATUS_DIR_NAME = "data_image_qda:status"
+  REDIS_INCR_NAME = "data_image_qda:max_id"
+  
+  def initialize
+    @redis = Redis.new(url:ENV['REDIS_URL'] || "redis://localhost:6379")
+  end
+  
+  def queue(lat, lng, multiply_const,
+      image_size, scale, 
+      quality_calc_method, quality_calc_value,
+      url, query)
+    id = @redis.incr REDIS_INCR_NAME
+    @redis.rpush REDIS_QUEUE_NAME, "#{id}
+#{lat}
+#{lng}
+#{multiply_const}
+#{image_size}
+#{scale}
+#{METHOD_MAP[quality_calc_method]}
+#{quality_calc_value}
+#{url}
+#{query}"
+
+  id
+  end
+
+  def status(id)
+  end
+
+end
