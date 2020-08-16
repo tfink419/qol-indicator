94556350bf45c39a2eb51b416b48c6cbbcb4a051
Tyler Fink <tfink419@gmail.com>
2020-08-15 19:07:48 -0600

    Increase num retries

--- app/jobs/build_quality_map_segment_job.rb
+++ app/jobs/build_quality_map_segment_job.rb
@@ -166,7 +166,7 @@ class BuildQualityMapSegmentJob < ApplicationJob
                     )
                   rescue DataImageCuda::TimeoutError
                     workers_service = GoogleWorkersService.new
-                    with_retries(max_tries: 4, rescue: DataImageCuda::TimeoutError) {
+                    with_retries(max_tries: 10, rescue: DataImageCuda::TimeoutError) {
                       dic.place_back_in_queue(id)
                       workers_service.check!
                       dic.wait_for(id)
