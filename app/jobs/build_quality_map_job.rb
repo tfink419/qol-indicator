857e4d01b2f2d039f52d059df67b636fc190501d
Tyler Fink <tfink419@gmail.com>
2020-09-01 18:54:14 -0600

    unless instead of if

--- app/jobs/build_quality_map_job.rb
+++ app/jobs/build_quality_map_job.rb
@@ -139,7 +139,7 @@ class BuildQualityMapJob < ApplicationJob
     @build_status.update!(error: "#{err.message}:\n#{err.backtrace}")
   ensure
     if @actually_ran
-      HerokuWorkersService.new.stop if BuildQualityMapStatus.most_recent
+      HerokuWorkersService.new.stop unless BuildQualityMapStatus.most_recent
       GoogleWorkersService.new.stop
     end
   end
