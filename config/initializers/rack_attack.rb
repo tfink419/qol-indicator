2c872e14ff8873ad243762f429b33f4bf075ce17
Tyler Fink <tfink419@gmail.com>
2021-09-13 22:20:48 -0600

    Adjust rack attack for map_data

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -27,7 +27,11 @@ class Rack::Attack
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
   throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
+    req.ip  unless req.path.start_with?('/assets') || req.path.start_with?('/map_data')
+  end
+
+  throttle('/map_data/ip', limit: 300, period: 1.minute) do |req|
+    req.ip if req.path.start_with?('/map_data')
   end
 
   ### Prevent Brute-Force Login Attacks ###
